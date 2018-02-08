592714473bc41d6a6a009881e54a9c50f0346b14
andrewhlu <alu@ucsb.edu>
2018-02-08 00:18:47 -0800

    Add continue button, remove all old comments

--- index.js
+++ index.js
@@ -91,120 +91,27 @@ function usernamesignin() {
 	});
 }
 
+//Already Signed In Function
+$(function() { //shorthand document.ready function
+    $('#alreadysignedin').on('submit', function(e) { //use on if jQuery 1.7+
+        e.preventDefault();  //prevent form from submitting
+        continuesignin();
+    });
+});
+
+function continuesignin() {
+	var redirectlink = "https://dvhsbluecrew.github.io/wfc/dashboard/index.html";
+	window.location.replace(redirectlink);
+}
+
 function signout() {
 	document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
 	alreadysignedin.setAttribute('hidden','true');
 	signinform.removeAttribute('hidden');
 }
 
-
-
-
 function getCookie(name) {
 	var re = new RegExp(name + "=([^;]+)");
 	var value = re.exec(document.cookie);
 	return (value != null) ? unescape(value[1]) : null;
 }
-
-
-
-
-//Begin BP Signin Code
-//Token Sign In
-// $(function() { //shorthand document.ready function
-//     $('#tokensignin').on('submit', function(e) { //use on if jQuery 1.7+
-//         e.preventDefault();  //prevent form from submitting
-//         tokensignin();
-//     });
-// });
-
-// function tokensignin() {
-// 	var modalTitle = document.getElementById('modalTitle');
-// 	var modalBody = document.getElementById('valid');
-
-// 	$("#myModal").modal();
-// 	modalTitle.innerHTML = 'Please Wait...';
-// 	modalBody.innerHTML = 'Signing you in...';
-
-// 	var accesstoken = document.getElementById('accesstoken').value;
-
-// 	var urlstring = "https://script.google.com/macros/s/AKfycbwf392-istjSgvNWVR10L_PFLbLhQuq8L-xr_3culH12E1NJko/exec?&token=" + accesstoken;
-
-// 	var settings = {
-// 	  "async": true,
-// 	  "crossDomain": true,
-// 	  "url": urlstring,
-// 	  "method": "GET"
-// 	}
-
-// 	$.ajax(settings).done(function (response) {
-// 	  if(response.error == 1) {
-// 	  	$("#myModal").modal();
-
-// 		modalTitle.innerHTML = 'Invalid Token!';
-// 	  	modalBody.innerHTML = 'The access token you provided is invalid. Please try again.';
-
-// 	  	document.getElementById("accesstoken").value = "";
-// 	  }
-// 	  else if(response.error == 4) {
-// 	  	$("#myModal").modal();
-
-// 	  	modalTitle.innerHTML = 'Expired Token!';
-// 	  	modalBody.innerHTML = 'Hi, ' + response.checkinstaff + '! You were inactive for more than two hours so you must obtain a new access token. Please sign in again with a new access token.';
-
-// 	  	document.getElementById("accesstoken").value = "";
-// 	  }
-// 	  else if(response.error == 3) {
-// 	  	var redirectlink = "https://dvhsbluecrew.github.io/scanner/scanner.html?token=" + accesstoken;
-// 	  	window.location.replace(redirectlink);
-// 	  }
-// 	});
-
-// 	return false;
-// }
-
-// //Username Sign In
-// $(function() { //shorthand document.ready function
-//     $('#fcsignin').on('submit', function(e) { //use on if jQuery 1.7+
-//         e.preventDefault();  //prevent form from submitting
-//         fcsignin();
-//     });
-// });
-
-// function fcsignin() {
-// 	var modalTitle = document.getElementById('modalTitle');
-// 	var modalBody = document.getElementById('valid');
-
-// 	$("#myModal").modal();
-// 	modalTitle.innerHTML = 'Please Wait...';
-// 	modalBody.innerHTML = 'Signing you in...';
-
-// 	var username = document.getElementById('username').value;
-// 	var password = document.getElementById('password').value;
-
-// 	var urlstring = "https://script.google.com/macros/s/AKfycbwDhsc4tJHfqDy9TC4Ku65SH7FOOOVGmqZZ7ETfRgCCpNOTy6Q/exec?&username=" + username + "&password=" + password;
-
-// 	var settings = {
-// 	  "async": true,
-// 	  "crossDomain": true,
-// 	  "url": urlstring,
-// 	  "method": "GET"
-// 	}
-
-// 	$.ajax(settings).done(function (response) {
-// 	  if(response.error == 1) {
-// 	  	$("#myModal").modal();
-
-// 		modalTitle.innerHTML = 'Invalid Username or Password!';
-// 	  	modalBody.innerHTML = 'The username and/or password you provided is invalid. Please try again.';
-
-// 	  	document.getElementById("password").value = "";
-// 	  }
-// 	  else if(response.error == 0) {
-// 	  	var redirectlink = "https://dvhsbluecrew.github.io/dashboard/index.html?token=" + response.token;
-// 	  	window.location.replace(redirectlink);
-// 	  }
-// 	});
-
-// 	return false;
-// }
