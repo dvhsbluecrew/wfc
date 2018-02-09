d67db9b44b82ca48c12491959c5fff3ce44a6181
andrewhlu <alu@ucsb.edu>
2018-02-09 01:56:39 -0800

    Add getCookie function

--- dashboard/accounts.js
+++ dashboard/accounts.js
@@ -341,3 +341,10 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+//Get cookie function
+function getCookie(name) {
+  var re = new RegExp(name + "=([^;]+)");
+  var value = re.exec(document.cookie);
+  return (value != null) ? unescape(value[1]) : null;
+}
