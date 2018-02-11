3fc1e6ef87dbb02adf9f0b9affd3419d5450d282
andrewhlu <alu@ucsb.edu>
2018-02-11 04:15:09 -0800

    Add bag search functionality

--- dashboard/accounts.js
+++ dashboard/accounts.js
@@ -221,35 +221,37 @@ function refreshtable() {
   gettabledata(token);
 }
 
-
-//begin old stuff
-
-//Form submit
+//Bag Search / Form submit
 $(function() { //shorthand document.ready function
-    $('#searchID').on('submit', function(e) { //use on if jQuery 1.7+
+    $('#searchBag').on('submit', function(e) { //use on if jQuery 1.7+
         e.preventDefault();  //prevent form from submitting
-        formsubmit();
+
+        var search = document.getElementById('search').value;
+        bagSearch(search);
     });
 });
 
-function formsubmit() {
-  var modaltitle = document.getElementById('modalTitle');
-  var valid = document.getElementById('valid');
-  var drinkpass = document.getElementById('drinkpass');
-  var guestpass = document.getElementById('guestpass');
+function bagSearch(bag) {
+  var bagNumber = document.getElementById('bagNumber');
+  var bagState = document.getElementById('bagState');
+  var bagBalance = document.getElementById('bagBalance');
+  var bagCheckout = document.getElementById('bagCheckout');
+  var bagReturn = document.getElementById('bagReturn');
+  var bagNotes = document.getElementById('bagNotes');
 
-  $("#myModal").modal();
+  bagNumber.innerHTML = 'Please Wait...';
+  bagState.innerHTML = '';
+  bagBalance.innerHTML = '';
+  bagCheckout.innerHTML = '';
+  bagReturn.innerHTML = '';
+  bagNotes.innerHTML = '';
 
-  modaltitle.innerHTML = 'Please Wait...';
-  valid.innerHTML = '';
-  drinkpass.innerHTML = '';
-  guestpass.innerHTML = '';
+  $('#studentModal').modal('hide');
+  $("#bagModal").modal();
 
-  //Get values from form
-  var idnumber = document.getElementById('search').value;
   var token = getCookie("token");
 
-  var urlstring = "https://script.google.com/macros/s/AKfycbzxPD0XVTHnUWMctHFjPiEzwnSX2CrFhtOqQux_6mAFT4cmbdsh/exec?id=" + idnumber + "&token=" + token;
+  var urlstring = "https://script.google.com/macros/s/AKfycbyKkt4S9bOnGHHYdtx5dqk3mRV3ckz0JJM88WXq_8IXlY77aJZc/exec?token=" + token + "&bag=" + bag + "&content=6";
 
   var settings = {
     "async": true,
@@ -259,41 +261,49 @@ function formsubmit() {
   }
 
   $.ajax(settings).done(function (response) {
-    modaltitle.innerHTML = response.name + ' (' + response.idnum + ')';
+    //console.log(response);
+    if(response.error == 0 || response.error == 6) {
+      bagNumber.innerHTML = "Bag #" + response.bag + " (" + response.name + ")";
 
-    if(response.error == 0) {
-      valid.innerHTML = '<div class="d-inline bg-success">Success! You checked the student in at ' + response.checkintime + '.</div>';
-
-      if(response.drinkpass2 == 1) {
-        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink pass.';
-      }
-      else if(response.drinkpass2 > 1) {
-        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink passes.';
-      }
-      if(response.guestpass > 0) {
-        guestpass.innerHTML = 'Student has a guest: ' + response.guestname + '. Please verify the guest\'s ID.';
+      if(response.timeout !== "") {
+        if(response.timereturned !== "") {
+          if(response.balance > 0) {
+            bagState.innerHTML = "This bag has been returned, but still carries a balance.";
+            bagBalance.innerHTML = "Remaining Balance: $" + response.balance.toFixed(2);
+            bagCheckout.innerHTML = response.timeout + ", by " + response.checkedoutby;
+            bagReturn.innerHTML = response.timereturned + ", by " + response.returnedby;
+          }
+          else {
+            bagState.innerHTML = "This bag has been returned and paid.";
+            bagBalance.innerHTML = "Remaining Balance: $" + response.balance.toFixed(2);
+            bagCheckout.innerHTML = response.timeout + ", by " + response.checkedoutby;
+            bagReturn.innerHTML = response.timereturned + ", by " + response.returnedby;
+          }
+        }
+        else {
+          bagState.innerHTML = "This bag has been checked out to " + response.name + ".";
+          bagBalance.innerHTML = "Remaining Balance: $" + response.balance.toFixed(2);
+          bagCheckout.innerHTML = response.timeout + ", by " + response.checkedoutby;
+          bagReturn.innerHTML = "N/A";
+        }
       }
     }
-    else if(response.error == 2) {
-      valid.innerHTML = '<div class="d-inline bg-warning">This student was checked in by ' + response.checkinstaff + ' at ' + response.checkintime + '.</div>';
-
-      if(response.drinkpass2 == 1) {
-        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink pass.';
-      }
-      else if(response.drinkpass2 > 1) {
-        drinkpass.innerHTML = 'Student has ' + response.drinkpass2 + ' drink passes.';
-      }
-      if(response.guestpass > 0) {
-        guestpass.innerHTML = 'Student has a guest: ' + response.guestname + '. Please verify the guest\'s ID.';
-      }
+    else if(response.error == 5) {
+      bagNumber.innerHTML = "Bag #" + response.bag;
+      bagState.innerHTML = "This bag has not been checked out."
+      bagBalance.innerHTML = '';
+      bagCheckout.innerHTML = "N/A";
+      bagReturn.innerHTML = "N/A";
     }
-    else if(response.error == 3){
-      valid.innerHTML = '<div class="d-inline bg-danger">We were unable to find a valid ticket with student ID #' + response.idnum + '.</div>';
-      drinkpass.innerHTML = 'Try scanning the card again.';
-    }
-    else {
+    else if(response.error == 1) {
       notloggedin();
     }
+    else{
+      bagState.innerHTML = 'Error (' + response.error + ')';
+      bagBalance.innerHTML = 'An error occurred. Please try again.';
+    }
+
+    bagNotes.innerHTML = response.notes;
   });
 
   return false;
